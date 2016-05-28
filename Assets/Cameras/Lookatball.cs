5c4d226c2321ee14ab873015ca25e522c5e7184b
BuildTools <unconfigured@null.spigotmc.org>
2016-05-28 01:07:33 -0500

    Camera update - fixed lookatball cam and 2 audio listener problem Revised max car turn rate (25 now 20) Made gameobjects prefabs for networking (yw alex) Skid particle effects realigned Event system added
    It's 1:30 AM and i have no life

--- Assets/Cameras/Lookatball.cs
+++ Assets/Cameras/Lookatball.cs
@@ -8,12 +8,14 @@ public class Lookatball : MonoBehaviour {
 	// Use this for initialization
 	
 	// Update is called once per frame
-	void FixedUpdate ()
+	void Update ()
     {
         directionToCar = (car.transform.position - target.transform.position) / (car.transform.position - target.transform.position).magnitude;
         directionToCar *= ((car.transform.position - target.transform.position).magnitude + 5);
         transform.position = target.transform.position + directionToCar;
         transform.position += new Vector3(3, 2.5f, 0);
+        if (transform.position.y <= 0)
+            transform.position += new Vector3(0, 0 - transform.position.y + 1, 0);
         transform.LookAt(target.transform.position);
 
 
