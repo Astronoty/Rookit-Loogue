ee5c21beb9c6183ee46715cbbe122440689a4c03
BuildTools <unconfigured@null.spigotmc.org>
2016-05-29 16:48:04 -0500

    UI Fixes Speedometer Added Explosion refined

---
+++ Assets/Car/Scripts/Speed.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class Speed : MonoBehaviour {
+    public Text speedDisplay;
+	
+	// Update is called once per frame
+	void Update () {
+        speedDisplay.text = "Speed: " + ((int)gameObject.GetComponent<Rigidbody>().velocity.magnitude).ToString();
+	}
+
+    
+}
